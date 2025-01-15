bb9e8c5a95bc5eca53567393d1cc25bd32a9c9e3
abubakrmisr <abubakrangular@gmail.com>
2025-01-15 03:06:46 +0200

    fixed all errors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,27 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  // imports: [RouterOutlet],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'mentoring-base';
+
+  readonly headerItem1 = 'Главная';
+
+  readonly headerItem2 = 'О компании';
+
+  readonly headerItem3 = 'Каталог';
+
+  readonly header2Item1 = 'Каталог';
+
+  readonly header2Item2 = 'Стройматериалы';
+
+  readonly header2Item3 = 'Инструменты';
+
+  readonly header2Item4 = 'Электрика';
+
+  readonly header2Item5 = 'Интерьр и одежда';
+
 }
