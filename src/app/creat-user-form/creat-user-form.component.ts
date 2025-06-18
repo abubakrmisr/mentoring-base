6af138eba2db4554ed22900a04ac86f94d380402
abubakrmisr <abubakrmisr@gmail.com>
2025-06-18 03:22:50 +0300

    fix: apply changes after review

--- src/app/creat-user-form/creat-user-form.component.ts
+++ src/app/creat-user-form/creat-user-form.component.ts
@@ -26,11 +26,11 @@ import { MatInputModule } from '@angular/material/input';
 })
 export class CreatUserFormComponent {
   public form = new FormGroup({
-    id: new FormControl<number>(new Date().getTime(), {nonNullable:true, validators: [Validators.required]}),
-    name: new FormControl('', {nonNullable:true, validators: [Validators.minLength(2), Validators.required]}),
-    email: new FormControl('', {nonNullable:true, validators: [Validators.email, Validators.required]}),
-    website: new FormControl('', {nonNullable:true, validators: [Validators.minLength(3), Validators.required]}),
-    companyName: new FormControl('', {nonNullable:true, validators: [Validators.minLength(2), Validators.required]}),
+    id: new FormControl<number>(new Date().getTime(), { nonNullable:true, validators: [Validators.required] }),
+    name: new FormControl('', { nonNullable:true, validators: [Validators.minLength(2), Validators.required] }),
+    email: new FormControl('', { nonNullable:true, validators: [Validators.email, Validators.required] }),
+    website: new FormControl('', { nonNullable:true, validators: [Validators.minLength(3), Validators.required] }),
+    companyName: new FormControl('', { nonNullable:true, validators: [Validators.minLength(2), Validators.required] }),
   });
 
   @Output()
